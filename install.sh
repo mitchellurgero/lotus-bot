45ce749d06c8f284e0c0c07ba1243857267c6158
Matthew <matthew@urgero.net>
2024-06-19 02:41:09 -0500

    Postgresql migration (#1)
    * Begin cutover to PostgreSQL
    
    Convert schema and `.env.example` config to PostgreSQL
    
    * Update `install.sh` script with PostgreSQL install

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #/bin/bash
-USER=lotus-bot
-INSTALLDIR=/opt
-HOMEDIR="$INSTALLDIR/lotus-bot"
+export USER="lotusbot"
+export INSTALLDIR="/opt"
+export HOMEDIR="$INSTALLDIR/lotusbot"
+
+# PostgreSQL details
+DB_HOST='127.0.0.1'
+DB_PORT='5432'
+DB_NAME='lotusbot'
+DB_SCHEMA='lotusbot'
+DB_USER='lotusbot'
+DB_PASS='lotusbot'
+DB_STRING="postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}"
 
 # Fail if not root
 if [ $(whoami) != "root" ];
@@ -10,15 +19,23 @@ then
   exit 1
 fi
 
+# Create the database
+sudo -u postgres -i <<EOF
+psql
+CREATE USER '${DB_USER}' ENCRYPTED PASSWORD '${DB_PASS}';
+CREATE SCHEMA '${DB_SCHEMA}' AUTHORIZATION '${DB_USER}';
+CREATE DATABASE '${DB_NAME}' OWNER '${DB_USER}';
+\q
+EOF
+
 # Set up and clone the repo
-mkdir -p $INSTALLDIR
-git clone https://github.com/givelotus/lotus-bot.git $HOMEDIR
+mkdir -p $HOMEDIR
+git clone https://github.com/LotusiaStewardship/lotus-bot.git $HOMEDIR
 
 # Create bot user
 useradd \
   --home-dir $HOMEDIR \
   --no-create-home \
-  --shell /bin/false \
   --system \
   $USER
 
@@ -26,11 +43,15 @@ useradd \
 GROUP=$(cat /etc/passwd | grep $USER | cut -d ':' -f 4)
 chown -R $USER:$GROUP $HOMEDIR
 
-# Run as bot user to set up repository and run INSTALL_COMMAND
-sudo -u $USER cp .env.example .env
-sudo -u $USER npm install
-sudo -u $USER npx prisma migrate dev --name init
-sudo -u $USER sqlite3 ./prisma/dev.db 'PRAGMA journal_mode=WAL; PRAGMA synchronous=FULL;'
+# Run as bot user to configure app and push database schema
+sudo -u $USER -i /bin/bash <<EOF
+cp .env.example .env
+sed -i 's/^DATABASE_URL/\#DATABASE_URL/' ".env"
+echo DATABASE_URL=\"${DB_STRING}\" >> .env
+npm install
+npx prisma generate
+npx prisma db push
+EOF
 
 # Install service files
 cp ./install/lotus-bot.service /etc/systemd/system
